04d5e55aa3c9cf10adb0ac815ab2747032411c35
Ben Kasper <bckasper@gmail.com>
2021-12-24 13:42:04 -0600

    added an authorization helper

---
+++ utils/authorization.js
@@ -0,0 +1,9 @@
+const authorization = (req, res, next) => {
+    if (!req.session.userId) {
+      res.redirect("/login");
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = authorization;
